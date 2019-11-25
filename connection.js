a53572ddb3503d6e4271569b69e65b604c27ecd8
waterylime46@aol.com <waterylime46@aol.com>
2019-11-25 11:55:05 -0700

    base of hw complete

---
+++ connection.js
@@ -0,0 +1,12 @@
+const util = require ('util')
+const mysql = require ('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Sootball12',
+    database: 'employees',
+})
+connection.connect()
+connection.query = util.promisify(connection.query)
+module.exports = connection
---
+++ seed.sql
@@ -0,0 +1,7 @@
+use employees;
+
+INSERT INTO department (name) VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Lead", 10000, 1), ("Sales Lead", 10000, 1),, ("Finance"), ("Legal");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES  ('John', 'Doe', 1, NULL);
