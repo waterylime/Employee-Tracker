6704b5aaaaa59a4c6fb3cb0e6b2cc8bc419d0997
waterylime46@aol.com <waterylime46@aol.com>
2019-12-03 12:00:30 -0700

    inquire added

--- connection.js
+++ connection.js
@@ -1,5 +1,6 @@
 const util = require ('util')
 const mysql = require ('mysql')
+const inquirer = require(“inquirer”);
 
 const connection = mysql.createConnection({
     host: 'localhost',
--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE role (
     department_id INT NOT NULL, 
     INDEX dep_ind (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
-)
+);
 
 
 CREATE TABLE employe (
@@ -24,8 +24,8 @@ CREATE TABLE employe (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL, 
     INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT,
     INDEX man_ind (manager_id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employe(id) ON DELETE CASCADE
-)
+);
